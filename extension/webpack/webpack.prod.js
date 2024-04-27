a5ce034b7b4b1ad1b515bad98c29a7ae54e9b9f5
Oein <oein0219@gmail.com>
2024-04-27 21:27:57 +0900

    chrome extension store policy

--- extension/webpack/webpack.prod.js
+++ extension/webpack/webpack.prod.js
@@ -1,6 +1,18 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'production'
+  mode: "production",
+  optimization: {
+    mangleExports: false,
+    mangleWasmImports: false,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: { compress: true, mangle: false },
+      }),
+    ],
+  },
 });
