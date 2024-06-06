daddc45a52e1e48f5c9b30f35d05daf1db108f0d
Oein <oein0219@gmail.com>
2024-06-06 19:58:13 +0900

    change desc

--- extension/src/types/global.d.ts
+++ extension/src/types/global.d.ts
@@ -1,16 +1,16 @@
 declare module "*.static.txt" {
-  const content: any;
+  const content: string;
   export default content;
 }
 declare module "*.static.css" {
-  const content: any;
+  const content: string;
   export default content;
 }
 declare module "*.static.html" {
-  const content: any;
+  const content: string;
   export default content;
 }
 declare module "*.static.data" {
-  const content: any;
+  const content: string;
   export default content;
 }
