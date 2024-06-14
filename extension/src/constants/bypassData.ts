0831c6a73da478cabf54e3f1eb96207441bf4f09
Oein <oein0219@gmail.com>
2024-06-14 23:41:41 +0900

    2.13

--- extension/src/constants/bypassData.ts
+++ extension/src/constants/bypassData.ts
@@ -1,6 +1,6 @@
 const bypassData = {
   bypassAgent:
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 <ChzzkExt>",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
   appVersion:
     "5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
   platform: "MacIntel",
