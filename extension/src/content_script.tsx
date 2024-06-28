732edf45a5104f7cb709dc1dd8718075a5b4ba26
Oein <oein0219@gmail.com>
2024-06-29 08:58:04 +0900

    fix fetching chzzext protocol for normal browser

--- extension/src/content_script.tsx
+++ extension/src/content_script.tsx
@@ -12,7 +12,7 @@ async function main() {
     lastConfig: "",
     configInstance,
   };
-  configInstance.syncConfig();
+  if (isElectron) configInstance.syncConfig();
   setupGlobalReciver();
   const apply = () => {
     if (
