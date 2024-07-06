ae04d7ae456add50db8132b46901a5d950b44e4b
Oein <oein0219@gmail.com>
2024-07-06 21:01:22 +0900

    fix: vod

--- extension/src/backgrounds/vod.ts
+++ extension/src/backgrounds/vod.ts
@@ -1,21 +1,5 @@
 import configInstance, { defaultConfig } from "@config";
-import { XMLParser } from "fast-xml-parser";
-
-class VideoSet {
-  quality: number;
-  frameRate: number;
-  BaseURL: string = "";
-
-  constructor(data: any) {
-    Object.assign(this, data);
-    [this.frameRate, this.quality] = data["nvod:Label"];
-  }
-
-  get data() {
-    const { BaseURL, quality, frameRate } = this;
-    return { BaseURL, quality, frameRate };
-  }
-}
+import { PlayBackData, VideoSet } from "./vodTypes";
 
 export default function vod() {
   chrome.webRequest.onCompleted.addListener(
@@ -29,52 +13,99 @@ export default function vod() {
       )
         return;
 
+      const tab = await chrome.tabs.get(requestDetails.tabId);
+      const tabId = requestDetails.tabId;
       const [{ result: exr }] = await chrome.scripting.executeScript({
         target: {
-          tabId: requestDetails.tabId,
+          tabId: tabId,
         },
         func: () => {
           return document.querySelector(`button[chzzkExt="voddl"]`) ||
-            document.body.getAttribute("chzzkExt_dl") != null
+            (document.body.getAttribute("chzzkExt_dl") != null &&
+              document.body.getAttribute("chzzkExt_dl") != "")
             ? true
             : false;
         },
       });
       if (exr) return;
 
-      const parser = new XMLParser();
-      const resp = await chrome.scripting.executeScript({
+      const taburl = new URL(tab.url!);
+      const vid = taburl.pathname.replace("/video/", "");
+
+      // https://api.chzzk.naver.com/service/v2/videos/26F8F68C7EAB2AF82B67C2262953FCCA04AD
+      // 여기에서 HLS Key를 Fetch함
+      const [
+        {
+          result: { content: videoInfo },
+        },
+      ] = await chrome.scripting.executeScript({
         target: {
-          tabId: requestDetails.tabId,
+          tabId,
         },
-        func: (video_url) => {
-          return fetch(video_url, {
-            credentials: "include",
-            mode: "cors",
+        func: (vid) => {
+          return fetch(`https://api.chzzk.naver.com/service/v2/videos/${vid}`, {
             method: "GET",
+            credentials: "include",
             headers: {
-              accept: "application/xml",
-              "accept-language": "ko,en;q=0.9,en-US;q=0.8,ja;q=0.7",
+              Cookie: document.cookie,
             },
-          }).then((resp) => resp.text());
+          }).then((resp) => resp.json());
         },
-        args: [requestDetails.url],
+        args: [vid],
       });
-      const [{ result }] = resp;
-      if (!result) return;
-      const body = parser.parse(result);
-      const items: VideoSet[] = body.MPD.Period.AdaptationSet.at(
-        0
-      ).Representation.map((el: VideoSet) => new VideoSet(el));
-      const urls = items
+
+      // https://apis.naver.com/neonplayer/vodplay/v1/playback/26F8F68C7EAB2AF82B67C2262953FCCA04AD?key=V128e4add35023e0040d004cc676647dae467d6cc2691a9b5b5a68486313024caed2e04cc676647dae467&sid=2099&env=real&st=5&lc=ko_KR&cpl=ko_KR
+      // 여기에서 PLAYBACK 요청하는듯
+      const [
+        {
+          result: { period: pldataArr },
+        },
+      ] = await chrome.scripting.executeScript({
+        target: {
+          tabId,
+        },
+        func: (vid, query) => {
+          return fetch(
+            `https://apis.naver.com/neonplayer/vodplay/v1/playback/${vid}?${query}`
+          ).then((res) => res.json());
+        },
+        args: [
+          videoInfo.videoId,
+          `key=${videoInfo.inKey}&env=real&lc=en_US&cpl=en_US`,
+        ],
+      });
+      // 예시 데이터는 plab.ts 참고
+      const playbackData = pldataArr[0] as PlayBackData;
+      const representations = playbackData.adaptationSet.map(
+        (el) => el.representation
+      );
+      const videoList: VideoSet[] = [];
+      representations.forEach((qarr) => {
+        qarr
+          .map((el) => {
+            const resolution = Object.fromEntries(
+              el.any.map((l) => [l.kind, l.value])
+            );
+            return {
+              quality: parseFloat(resolution.resolution),
+              frameRate: parseFloat(resolution.fps),
+              BaseURL: el.baseURL.find((el) => el.value.includes(".mp4"))
+                ?.value!,
+            };
+          })
+          .filter((el) => el.BaseURL)
+          .forEach((el) => videoList.push(el));
+      });
+
+      const urls = videoList
         .sort((a, b) =>
           b.quality - a.quality
             ? b.quality - a.quality
             : b.frameRate - a.frameRate
         )
-        .filter((el) => el.BaseURL.startsWith("https://"))
-        .map((el) => el.data);
+        .filter((el) => el.BaseURL.startsWith("https://"));
       const url_stringify = JSON.stringify(urls);
+
       await chrome.scripting.executeScript({
         target: {
           tabId: requestDetails.tabId,
