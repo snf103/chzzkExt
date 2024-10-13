007094f6061f4ad3b0ec688b733c671d3f59dfc7
Oein <oein0219@gmail.com>
2024-10-13 16:23:37 +0900

    fix

--- extension/src/components/chat/initReverseChat.ts
+++ extension/src/components/chat/initReverseChat.ts
@@ -1,14 +1,8 @@
+import st from "#s/reverse.static.css";
+import styler from "@/utils/styler";
 export default function initReverseChat(enable: boolean) {
   const interval = () => {
-    const container = document.querySelector(
-      `section[class*=live_container] > [class*=live_wrapper]`
-    ) as HTMLDivElement;
-    if (!container) return;
-    if (enable) {
-      container.style.flexDirection = "row-reverse";
-    } else {
-      container.style.flexDirection = "row";
-    }
+    styler("reverse", st, enable);
   };
 
   if (typeof window.chzzkExt.revchat != "undefined") {
